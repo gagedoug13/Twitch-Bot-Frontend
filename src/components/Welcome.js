369e6d85f3ee6538e509deb0da999bb4e9f896e4
Gage Thornberry <gagedoug13@gmail.com>
2020-06-16 23:34:19 -0600

    changing redirect to login

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 export default function Welcome() {
+    const twitchLoginUrl = ''
+
     return (
         <div className='welcomeDiv'>
             <h1 className='welcomeMessage'>To get started</h1>
-            <a href='https://twitch.com'><button className='signInButton'>Sign in with Twitch.</button></a>
+            <a href={twitchLoginUrl}><button className='signInButton'>Sign in with Twitch.</button></a>
         </div>
     )
 }
