973f9571a127019743602ddc5e849a45c56a1bf8
Gage Thornberry <gagedoug13@gmail.com>
2020-06-17 03:21:49 -0600

    redirecting to twitch login page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Welcome() {
-    const twitchLoginUrl = ''
+    const twitchLoginUrl = 'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=fshwwwtjkuk4x4xda6wiz4mcfenicw&redirect_uri=http://localhost:3002&scope=channel:read:redemptions+user:read:email'
 
     return (
         <div className='welcomeDiv'>
